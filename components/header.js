4509b4aec304ce71431af4b4cac8e2d49a0a9498
osten <ostenda95@gmail.com>
2024-01-14 21:23:35 +0000

    header v1

--- components/header.js
+++ components/header.js
@@ -3,23 +3,28 @@ import Image from 'next/image'
 export default function Header() {
 
     return (
-        <div
+    <div
         className={
             "container mx-auto flex items-center border-b-2 px-6 py-2 h-24"
         }
        >
 
-        <h1 className ="font=bold">KS HEKSA</h1>
+        <h1 className ="font=bold flex-grow">
+        <img 
+            class='h-20 object-contain' 
+            src='./img/logo.jpg' 
+            alt='KS HEKSA'
+             />
+        </h1>
        <div className="grow">
             <div className='flex item-center justity-center gap-2 md:gap-8'>
-            <a href="#">Home</a>
-            <a href="#">Blog</a>
-            <a href="#">Shop</a>
-            <a href="#">About</a>
-            <a href="#">Contact</a>
+                <a href="#">STRONA GŁÓWNA</a>
+                <a href="#">AKTUALNOŚCI</a>
+                <a href="#">KLUB</a>
+                <a href="#">KONTAKT</a>
             </div>
-       </div>
-       </div>
+        </div>
+    </div>
     )
 };
 
